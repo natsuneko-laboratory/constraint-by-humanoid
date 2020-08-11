f5d36e758bb2d05ac0468af681911a3c25cab5d8
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-11 20:07:31 +0900

    feat: implemented

--- Assets/Mochizuki/ConstraintByHumanoid/Editor/ConstraintEditor.cs
+++ Assets/Mochizuki/ConstraintByHumanoid/Editor/ConstraintEditor.cs
@@ -30,6 +30,11 @@ namespace Assets.Mochizuki.ConstraintByHumanoid
         [SerializeField]
         private List<string> _errors;
 
+        [SerializeField]
+        private GameObject[] _excludes;
+
+        private bool _isShowExcludeSettings;
+
         private GameObject _src;
 
         public ConstraintEditor()
@@ -43,7 +48,7 @@ namespace Assets.Mochizuki.ConstraintByHumanoid
                          .ToArray();
         }
 
-        [MenuItem("Mochizuki/Unity/Constraint by Humanoid")]
+        [MenuItem("Mochizuki/Constraint by Humanoid")]
         public static void ShowWindow()
         {
             var window = GetWindow<ConstraintEditor>();
@@ -67,6 +72,38 @@ namespace Assets.Mochizuki.ConstraintByHumanoid
 
             _src = ObjectPicker("Source GameObject", _src);
             _dst = ObjectPicker("Destination GameObject", _dst);
+
+            _isShowExcludeSettings = EditorGUILayout.Foldout(_isShowExcludeSettings, "Exclude Components");
+            if (_isShowExcludeSettings)
+            {
+                EditorGUI.indentLevel++;
+
+                PropertyField(this, nameof(_excludes));
+
+                var area = GUILayoutUtility.GetRect(0.0f, 50.0f, GUILayout.ExpandWidth(true));
+                GUI.Box(area, "Drag and Drop GameObjects that you want to exclude");
+
+                if (area.Contains(Event.current.mousePosition))
+                    switch (Event.current.type)
+                    {
+                        case EventType.DragUpdated:
+                            DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                            Event.current.Use();
+                            break;
+
+                        case EventType.DragPerform:
+                            DragAndDrop.AcceptDrag();
+
+                            var references = DragAndDrop.objectReferences;
+                            _excludes = (_excludes ?? Array.Empty<GameObject>()).Concat(references).Distinct().Cast<GameObject>().ToArray();
+
+                            DragAndDrop.activeControlID = 0;
+                            Event.current.Use();
+                            break;
+                    }
+
+                EditorGUI.indentLevel--;
+            }
 
             _constraint = (Constraint) EditorGUILayout.Popup(new GUIContent("Constraint"), (int) _constraint, _items);
 
@@ -94,7 +131,7 @@ namespace Assets.Mochizuki.ConstraintByHumanoid
             EditorGUI.BeginDisabledGroup(_errors.Count > 0);
 
             if (GUILayout.Button("変更を適用"))
-                ApplyChanges(_src, _dst, _constraint);
+                ApplyChanges(_src, _dst, _excludes, _constraint);
 
             EditorGUI.EndDisabledGroup();
         }
@@ -110,80 +147,80 @@ namespace Assets.Mochizuki.ConstraintByHumanoid
             return errors;
         }
 
-        private static void ApplyChanges(GameObject src, GameObject dst, Constraint constraint)
+        private static void ApplyChanges(GameObject src, GameObject dst, GameObject[] excludes, Constraint constraint)
         {
-            ConfigureConstraint(src, dst, constraint);
+            ConfigureConstraint(src, dst, excludes, constraint);
         }
 
-        private static void ConfigureConstraint(GameObject src, GameObject dst, Constraint constraint)
+        private static void ConfigureConstraint(GameObject src, GameObject dst, GameObject[] excludes, Constraint constraint)
         {
             var s = src.GetComponent<Animator>();
             var d = dst.GetComponent<Animator>();
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.Hips);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.Spine);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.Chest);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.UpperChest);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.Hips);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.Spine);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.Chest);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.UpperChest);
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftShoulder);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftUpperArm);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftLowerArm);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftHand);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftShoulder);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftUpperArm);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftLowerArm);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftHand);
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightShoulder);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightUpperArm);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightLowerArm);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightHand);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightShoulder);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightUpperArm);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightLowerArm);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightHand);
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftUpperLeg);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftLowerLeg);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftFoot);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftToes);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftUpperLeg);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftLowerLeg);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftFoot);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftToes);
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightUpperLeg);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightLowerLeg);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightFoot);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightToes);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightUpperLeg);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightLowerLeg);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightFoot);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightToes);
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.Neck);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.Head);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftEye);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightEye);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.Neck);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.Head);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftEye);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightEye);
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftThumbProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftThumbIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftThumbDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftIndexProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftIndexIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftIndexDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftMiddleProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftMiddleIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftMiddleDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftRingProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftRingIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftRingDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftLittleProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftLittleIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.LeftLittleDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftThumbProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftThumbIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftThumbDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftIndexProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftIndexIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftIndexDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftMiddleProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftMiddleIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftMiddleDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftRingProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftRingIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftRingDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftLittleProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftLittleIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.LeftLittleDistal);
 
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightThumbProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightThumbIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightThumbDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightIndexProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightIndexIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightIndexDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightMiddleProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightMiddleIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightMiddleDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightRingProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightRingIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightRingDistal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightLittleProximal);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightLittleIntermediate);
-            ConfigureRotationConstraintToGameObject(s, d, constraint, HumanBodyBones.RightLittleDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightThumbProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightThumbIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightThumbDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightIndexProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightIndexIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightIndexDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightMiddleProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightMiddleIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightMiddleDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightRingProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightRingIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightRingDistal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightLittleProximal);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightLittleIntermediate);
+            ConfigureRotationConstraintToGameObject(s, d, excludes, constraint, HumanBodyBones.RightLittleDistal);
         }
 
-        private static void ConfigureRotationConstraintToGameObject(Animator src, Animator dst, Constraint type, HumanBodyBones id)
+        private static void ConfigureRotationConstraintToGameObject(Animator src, Animator dst, GameObject[] excludes, Constraint type, HumanBodyBones id)
         {
             var srcBone = src.GetBoneTransform(id);
             var dstBone = dst.GetBoneTransform(id);
@@ -193,6 +230,9 @@ namespace Assets.Mochizuki.ConstraintByHumanoid
 
             var srcGameObject = srcBone.gameObject;
             var dstGameObject = dstBone.gameObject;
+
+            if (excludes.Contains(srcGameObject) || excludes.Contains(dstGameObject))
+                return;
 
             var constraint = AddConstraintToGameObject(dstGameObject, type);
             var source = new ConstraintSource { sourceTransform = srcGameObject.transform, weight = 1.0f };
@@ -225,6 +265,16 @@ namespace Assets.Mochizuki.ConstraintByHumanoid
                 default:
                     throw new ArgumentOutOfRangeException(nameof(constraint), constraint, null);
             }
+        }
+
+        public static void PropertyField(EditorWindow editor, string property)
+        {
+            var so = new SerializedObject(editor);
+            so.Update();
+
+            EditorGUILayout.PropertyField(so.FindProperty(property), true);
+
+            so.ApplyModifiedProperties();
         }
 
         private static T ObjectPicker<T>(string label, T obj) where T : Object
