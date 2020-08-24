5c4113fd86747aef7993fa7601e0f2ae3e1ffeed
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-08-24 12:45:58 +0900

    chore: add GitHub Actions Workflow

---
+++ Assets/Mochizuki/ConstraintByHumanoid/Packaging/AutomatedRelease.cs
@@ -0,0 +1,12 @@
+﻿using Mochizuki.VariationPackager;
+
+namespace Mochizuki.ConstraintByHumanoid.Packaging
+{
+    public static class AutomatedRelease
+    {
+        public static void Build()
+        {
+            CLI.BuildWithScene("Scenes/Release.unity", true);
+        }
+    }
+}
